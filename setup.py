a4f93b5c8942bc7b3d4deaef4ebec28dc69b4bf2
Eric Burdett <eric@burdett.us>
2020-10-20 20:14:19 -0600

    Updated setup.py and meta.yaml for conda packaging

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
     package_data={
         'hwr': 'wbs/data/*'
     },
-    install_requires=['tensorflow', 'pandas', 'matplotlib', 'pyyaml', 'numpy', 'editdistance', 'tqdm']
+    install_requires=['tensorflow-gpu', 'pandas', 'matplotlib', 'pyyaml', 'numpy', 'editdistance', 'tqdm']
 )
